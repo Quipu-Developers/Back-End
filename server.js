7d1b0346eb5cb30eb7c90cdb4607267f5f6b5366
ejjem <tigonlee0627@gmail.com>
2024-07-26 14:37:29 +0900

    add swagger

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const path = require('path');
 const mysql = require('mysql2');
 const app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 7777;
+const PORT = process.env.PORT || 3001;
 const morgan = require('morgan');
 
 const { sequelize } = require('./models');
 const uploadRouter = require('./routes/upload.js');
 const upload2Router = require('./routes/upload2.js');
+
+// Swagger 관련 추가
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 //morgan
 app.use(morgan('dev'));
 // JSON 데이터를 파싱하기 위한 미들웨어 설정
@@ -27,7 +32,8 @@ sequelize.authenticate()
     .then(() => {
         console.log('DB 동기화');
         app.listen(PORT, () => {
-            console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+            console.log(`port:${PORT}`)
+            console.log(`swagger: http://localhost:${PORT}/api-docs`);
         });
     })
     .catch(err => {
@@ -37,6 +43,8 @@ sequelize.authenticate()
 // POST 요청을 처리 by upload route
 app.use('/data1', uploadRouter);
 app.use('/data2', upload2Router);
+//swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //error 처리
 app.use((err, req, res, next) => {
     console.log(err);
