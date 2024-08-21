29093cbcc40c7746c0bb3e742d7907a79e87e5e0
ejjem <tigonlee0627@gmail.com>
2024-08-21 17:32:53 +0900

    upload2.js 버그 수정

--- src/routes/upload2.js
+++ src/routes/upload2.js
@@ -22,7 +22,10 @@ const upload = multer({
             done(null, portfolioDir); //portfolio 디렉토리에 저장
         },
         filename(req, file, done) {
-            done(null, file.originalname);
+            const timestamp = Date.now();
+            const ext = path.extname(file.originalname);
+            const basename = path.basename(file.originalname, ext);
+            done(null, `${basename}-${timestamp}${ext}`); // 타임스탬프를 파일 이름에 추가
         }
     }),
 })
@@ -48,10 +51,9 @@ router.post('/', async (req, res) => {
             const { name, student_id, major, phone_number, motivation, department, project_description,
                 github_profile, github_email, slack_email, willing_general_member } = req.body;
 
-            //pdf 이름 변경
             const ext = path.extname(req.file.originalname);
-            const portfolioPdfFilename = `퀴푸-[${department}]${student_id}${name}` + ext;
-            fs.renameSync(req.file.path, path.join(portfolioDir, portfolioPdfFilename));
+            const tmpFilename = req.file.filename;
+            const tmpFilepath = path.join(portfolioDir, tmpFilename);
 
 
             // 값 누락 체크
@@ -59,14 +61,14 @@ router.post('/', async (req, res) => {
             const requiredFields_dev = {project_description, github_profile, github_email, slack_email}
             for (const [field, value] of Object.entries(requiredFields)) {
                 if (!value) {
-                    deletefile(res, path.join(portfolioDir, portfolioPdfFilename));
+                    deletefile(res, tmpFilepath);
                     return res.status(400).send(sendingerror(field, 1));
                 }
             }
             if (department !== 'design'){
                 for (const [field, value] of Object.entries(requiredFields_dev)) {
                     if (!value) {
-                        deletefile(res, path.join(portfolioDir, portfolioPdfFilename));
+                        deletefile(res, tmpFilepath);
                         return res.status(400).send(sendingerror(field, 1));
                     }
                 }
@@ -76,7 +78,7 @@ router.post('/', async (req, res) => {
             for (const [field, validator] of Object.entries(validators)) {
                 const value = req.body[field];
                 if (value !== '' && !validator(req.body[field])) {
-                    deletefile(res, path.join(portfolioDir, portfolioPdfFilename));
+                    deletefile(res, tmpFilepath);
                     return res.status(400).send(sendingerror(field, 2));
                 }
             }
@@ -84,12 +86,22 @@ router.post('/', async (req, res) => {
             // 중복 확인 by student_id
             const Check = await Dev_member.findOne({ where: { student_id } });
             if (Check) {
-                deletefile(res, path.join(portfolioDir, portfolioPdfFilename));
+                deletefile(res, tmpFilepath);
                 return res.status(409).send(`이미 신청하셨습니다`);
             }
             console.log('데이터 검사 완료');
 
-            // 문제 없으면 저장
+
+            // 문제 없으면 pdf 이름 변경 후 저장
+            const portfolioPdfFilename = `퀴푸-[${department}]${student_id}${name}` + ext;
+            try {
+                fs.renameSync(tmpFilepath, path.join(portfolioDir, portfolioPdfFilename));
+                console.log(`파일 저장: ${portfolioPdfFilename}`);
+            } catch(err){
+                console.log(err);
+                console.log(`파일 저장 실패: ${portfolioPdfFilename}`);
+            }
+
             await Dev_member.create({
                 name,
                 student_id,
