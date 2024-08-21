f295f0c79ec71674c0b3a8b3695ba1f27dbe44a4
ejjem <tigonlee0627@gmail.com>
2024-08-21 15:05:52 +0900

    주석 정리

--- src/app.js
+++ src/app.js
@@ -55,19 +55,6 @@ sequelize.authenticate()
     .catch(err => {
         console.error('DB 연결 실패:', err);
     });
-//cors 정책 해결
-/*
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://quipu.uos.ac.kr');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200); // preflight 요청에 대한 응답
-    }
-    next();
-});
- */
 
 //router 처리
 app.use('/data1', uploadRouter);
